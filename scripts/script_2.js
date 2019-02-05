ee61772b5d2aba8fe982b56234312cd594b49573
sl1nk4991 <47352229+sl1nk4991@users.noreply.github.com>
2019-02-05 21:43:14 +0300

    new_files_1.0
    update_1.0

---
+++ scripts/script_2.js
@@ -0,0 +1,12 @@
+var dark = true;
+
+function change_theme() {
+	dark = !dark;
+	if (dark == true) {
+		document.getElementById("theme").setAttribute("href", "../style/style.css");
+		document.getElementById("chng_button").innerHTML='<a onclick="change_theme()" class="button18" tabindex="0">Light Theme</a>';
+	} else {
+		document.getElementById("theme").setAttribute("href", "../style/style_w.css");
+		document.getElementById("chng_button").innerHTML='<a onclick="change_theme()" class="button18" tabindex="0">Dark Theme</a>';
+	}  
+}
